e3391bf4b6bf4559db57643c867addb400f2f0c4
alin_alok <alokmahtab88@gmail.com>
2021-11-16 21:57:55 +0600

    4th problem and solve added

--- exercise-1.py
+++ exercise-1.py
@@ -6,13 +6,14 @@ def even_indx(String):
     for i in range(0,size-1,2):
         print("Index number :",i," ",String[i])
 print(even_indx(String))
-#Solve-2
-String = input("Enter the String: ")
-inputString = list(String)
-for i in inputString[0::2]:
-    print(i)
-#Problem-3: Remove any of the first character from a string
-#solve-1
+# #Solve-2
+# String = input("Enter the String: ")
+# inputString = list(String)
+# for i in inputString[0::2]:
+#     print(i)
+    
+# #Problem-2: Remove any of the first character from a string
+# #solve-1
 input_string=input("Enter the String : ")
 input_int =int(input("Enter the number of string to remove : "))
 def character_remove(input_string,input_int):
@@ -21,3 +22,22 @@ def character_remove(input_string,input_int):
     return slice_method
 print("remove first character")
 print(character_remove(input_string,input_int))
+
+#problem-3: check the first and last number of the list matches or not
+#solve-1
+def check_first_last_num(number_list):
+    print("This is the given numbers: ",number_list)
+    first_numb = number_list[0]
+    last_numb= number_list[-1]
+    while first_numb == last_numb:
+        return True
+    else:
+        return False
+        
+list1 = []
+n = int(input("Enter number of elements : "))
+for i in range(0, n):
+    element = int(input("enter numbers into list: "))
+    list1.append(element) # adding the element in the empty list1[]
+list_as_input=list1
+print("Answer is ",check_first_last_num(list_as_input))
