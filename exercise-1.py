90bd4949e4018264d0c91c9cc0c006228c5749c0
alin_alok <alokmahtab88@gmail.com>
2021-11-16 19:46:14 +0600

    3rd problem and solve added

--- exercise-1.py
+++ exercise-1.py
@@ -7,8 +7,17 @@ def even_indx(String):
         print("Index number :",i," ",String[i])
 print(even_indx(String))
 #Solve-2
-#String = input("Enter the String: ")
-#inputString = list(String)
-#for i in inputString[0::2]:
-#    print(i)
- 
+String = input("Enter the String: ")
+inputString = list(String)
+for i in inputString[0::2]:
+    print(i)
+#Problem-3: Remove any of the first character from a string
+#solve-1
+input_string=input("Enter the String : ")
+input_int =int(input("Enter the number of string to remove : "))
+def character_remove(input_string,input_int):
+    print("This is the main string ",input_string)
+    slice_method=input_string[input_int:]
+    return slice_method
+print("remove first character")
+print(character_remove(input_string,input_int))
